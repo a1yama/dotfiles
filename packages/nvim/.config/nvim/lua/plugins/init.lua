760e5335d9ee6e7dd83654fb4f1a7decde5ddd03
a1yama <a1yama1123@gmail.com>
2025-02-11 12:37:32 +0900

    update

--- packages/nvim/.config/nvim/lua/plugins/init.lua
+++ packages/nvim/.config/nvim/lua/plugins/init.lua
@@ -2,16 +2,42 @@ return {
     -- Neo-tree.nvim
     { 
       "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
       dependencies = {
-        "nvim-lua/popup.nvim",
-        "nvim-tree/nvim-web-devicons",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
       },
+      opts = {
+        filesystem = {
+            follow_current_file = { enabled = true },
+            hijack_netrw_behavior = "open_current",
+            use_libuv_file_watcher = true,
+            filtered_items = {
+              visible = false, -- デフォルトで隠されているかどうか
+              show_hidden_count = true,
+              hide_dotfiles = false, -- dotfileを隠すかどうか
+              hide_gitignored = false, -- gitignoreされているファイルを隠すかどうか
+              hide_by_name = {
+                "node_modules",
+                "thumbs.db",
+              },
+              never_show = {
+                ".git",
+                ".DS_Store",
+                ".history",
+              },
+            },
+          },
+      }
     },
   
     -- telescope.nvim
     { 
       "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/popup.nvim" },
+      tag = '0.1.8',
+      dependencies = { "nvim-lua/plenary.nvim" },
     },
   
     -- lualine.nvim
