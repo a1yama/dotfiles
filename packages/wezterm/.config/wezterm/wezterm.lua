ec0664048328effe56d304f8b067571aeb3e0e65
a1yama <a1yama1123@gmail.com>
2026-01-30 19:00:56 +0900

    update

--- packages/wezterm/.config/wezterm/wezterm.lua
+++ packages/wezterm/.config/wezterm/wezterm.lua
@@ -128,7 +128,8 @@ end)
 config.disable_default_key_bindings = true
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
-config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
+-- Leader key disabled to avoid tmux conflict
+-- config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
 
